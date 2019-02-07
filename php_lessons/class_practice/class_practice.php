e010e2f3d3155c455c4021ae72d1f7b913c2d50f
fuji <fuji@interwroks.jp>
2019-02-07 19:45:54 +0900

    :shirt: インデント修正

--- php_lessons/class_practice/class_practice.php
+++ php_lessons/class_practice/class_practice.php
@@ -1,26 +1,23 @@
 <?php
+	// User
+	class User {
+  		// property
+  		public $name;
 
-// User
+  		// constructor
+  		public function __construct($name) {
+    		$this->name = $name;
+  		}
 
-class User {
-  // property
-  public $name;
+  		// method
+  		public function sayHi() {
+    		echo "hi, i am $this->name!";
+  		}
+	}
 
-  // constructor
-  public function __construct($name) {
-    $this->name = $name;
-  }
+	$tom = new User("Tom");
+	$bob = new User("Bob");
 
-  // method
-  public function sayHi() {
-    echo "hi, i am $this->name!";
-  }
-}
-
-$tom = new User("Tom");
-$bob = new User("Bob");
-
-echo $tom->name; // Tom
-$bob->sayHi(); // hi, i am Bob!
-
+	echo $tom->name; // Tom
+	$bob->sayHi(); // hi, i am Bob!
 ?>
